1210e2f5bfb10a80f60dfcd8c5b294154eae6f3f
Zeyad <zeyadw526@gmail.com>
2025-07-18 21:36:50 +0300

    create and retreive data from the db

--- db/tasks.go
+++ db/tasks.go
@@ -1,22 +1,89 @@
 package db
 
 import (
+	"encoding/binary"
 	"time"
 
 	"github.com/boltdb/bolt"
 )
 
 var taskBucket = []byte("tasks")
+var db *bolt.DB
+
+type Task struct {
+	Key   int
+	Value string
+}
 
 // This is not same as the init() go function. This is a normal function that is exported to be used in an package.
 func Init(dbPath string) error {
-	db, err := bolt.Open(dbPath, 0600, &bolt.Options{Timeout: 1 * time.Second})
+	// Creating a boltdb connection. `db`
+	var err error
+	db, err = bolt.Open(dbPath, 0600, &bolt.Options{Timeout: 1 * time.Second})
 	if err != nil {
 		return err
 	}
 
+	// Using the read-write transaction to add it is not existed!
 	return db.Update(func(tx *bolt.Tx) error {
 		_, err := tx.CreateBucketIfNotExists(taskBucket)
 		return err
 	})
+}
+
+func CreateTask(task string) (int, error) {
+	var id int
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		id64, _ := b.NextSequence()
+
+		id = int(id64)
+		key := itob(id)
+
+		err := b.Put(key, []byte(task))
+		return err
+	})
+
+	if err != nil {
+		return -1, err
+	}
+
+	return id, nil
+}
+
+func ReadTasks(ids []int) ([]Task, error) {
+	tasks := []Task{}
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		c := b.Cursor()
+
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			tasks = append(tasks, Task{
+				Key:   btoi(k),
+				Value: btos(v),
+			})
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
+
+func itob(v int) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(v))
+	return b
+}
+
+func btoi(b []byte) int {
+	return int(binary.BigEndian.Uint64(b))
+}
+
+func btos(b []byte) string {
+	return string(b)
 }
